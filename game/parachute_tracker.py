e56393a2769821f849804cf024a816c83ccc663c
Kelton Green <greenkelton@protonmail.com>
2021-05-22 14:24:39 -0600

    fixed to fit with Agnes's Director class...I think

--- game/parachute_tracker.py
+++ game/parachute_tracker.py
@@ -6,34 +6,8 @@ class Parachute_Tracker:
         Args:
             self (Parachute): An instance of Parachute.
         """
-        self.strikes = 0
-        self.correct = False
-        self.letter = []
-
-    
-    def update_strikes(self):
-        """Adds a strike for each wrong answer.
-
-        Args:
-            self (Parachute): An instance of Parachute.
-        
-        Returns:
-            Int: strikes up to 4.
-        """
-        if self.correct == True:
-            self.strikes += 1
-        
-    def is_correct(self):
-        """Determines if the letter was guessed correctly or not
-
-        Args:
-            self (Parachute): an instance of Parachute.
-
-        Returns:
-            boolean: True if the letter is in the word; False if it is not.
-        """
-        if self.letter[0] == letter_in_word: 
-            return True
+        self.state_num = 0
+        self.keep_playing = True
 
     def get_parachute(self):
         """Prints the parachute according to how many strikes there are.
@@ -45,12 +19,23 @@ class Parachute_Tracker:
             string: the amount of parachute that is left.
         """
         parachute = "\n  ___  \n /___\  \n \   / \n  \ /  \n   0   \n  /|\  \n  / \  \n \n^^^^^^^"
-        if self.strikes == 1:
-            parachute = "\n  /___\  \n \   / \n  \ /  \n   0   \n  /|\  \n  / \  \n \n^^^^^^^"
-        elif self.strikes == 2:
+        if self.state_num == 1:
+            parachute = "\n /___\  \n \   / \n  \ /  \n   0   \n  /|\  \n  / \  \n \n^^^^^^^"
+        elif self.state_num == 2:
             parachute = "\n \   / \n  \ /  \n   0   \n  /|\  \n  / \  \n \n^^^^^^^"
-        elif self.strikes == 3:
+        elif self.state_num == 3:
             parachute = "\n  \ /  \n   0   \n  /|\  \n  / \  \n \n^^^^^^^"
-        elif self.strikes == 4:
+        elif self.state_num == 4:
             parachute = "\n   X   \n  /|\  \n  / \  \n \n^^^^^^^"
         return parachute
+
+    def game_continue(self):
+        """determines if the game should continue
+
+        Args:
+            self (Parachute): an instance of Parachute
+        Returns:
+            boolean
+        """
+        if self.state_num == 4:
+            return self.keep_playing == False
