2286e56a1c1265eee01ff40ac41e3f27c8b5d822
Sarah Adams <sarahadams2718@gmail.com>
2021-05-21 21:04:31 -0600

    Words_Select class and method. returns a single word string.

--- __main__.py
+++ __main__.py
@@ -1,4 +1,20 @@
-from game.director import Director
+import random
 
-director = Director()
-director.start_game()
+class Word_Select:
+
+    def Word_Select():
+        # reads the file and splits it into a list
+        word_file = open("wordlist.10000.txt", "r")
+        content = word_file.read()
+        content_list = content.split()
+        # returns 1 picked word 
+        return random.sample(content_list,1)
+
+
+
+
+
+
+
+
+
