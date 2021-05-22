75ac223d66a639cfde89f14b8e2d87f6a6db5f84
Kelton Green <greenkelton@protonmail.com>
2021-05-21 23:18:20 -0600

    forgot to save file - actual first commit

--- game/parachute_tracker.py
+++ game/parachute_tracker.py
@@ -1,6 +1,6 @@
 import random
 
-class Parachute:
+class Parachute_Tracker:
 
     def __init__(self):
         """Class constructor. Declares and initializes instance attributes.
@@ -9,6 +9,8 @@ class Parachute:
             self (Parachute): An instance of Parachute.
         """
         self.strikes = 0
+        self.correct = False
+        self.letter = []
     
     def update_strikes(self):
         """Adds a strike for each wrong answer.
@@ -19,7 +21,20 @@ class Parachute:
         Returns:
             Int: strikes up to 4.
         """
+        if self.correct == True:
+            self.strikes += 1
         
+    def is_correct(self):
+        """Determines whether the letter guessed is a letter in the word or not.
+
+        Args:
+            self (Parachute): an instance of Parachute.
+
+        Returns:
+            boolean: True if the letter is in the word; False if it is not.
+        """
+        if self.letter[0] == letter_in_word: 
+            return True
 
     def get_parachute(self):
         """Prints the parachute according to how many strikes there are.
@@ -30,12 +45,43 @@ class Parachute:
         Returns:
             string: the amount of parachute that is left.
         """
-    
-    def is_correct(self):
-        """Determines whether the letter guessed is a letter in the word or not.
-
-        Args:
-            self (Parachute): an instance of Parachute.
-
-        Returns:
-            boolean: True if the letter is in the word; False if it is not.
+        if self.strikes == 0: 
+            print("  ___  ")
+            print(" /___\ ")
+            print(" \   / ")
+            print("  \ /  ")
+            print("   0   ")
+            print("  /|\  ")
+            print("  / \  ")
+            print()
+            print("^^^^^^^")
+        elif self.strikes == 1:
+            print(" /___\ ")
+            print(" \   / ")
+            print("  \ /  ")
+            print("   0   ")
+            print("  /|\  ")
+            print("  / \  ")
+            print()
+            print("^^^^^^^")
+        elif self.strikes == 2:
+            print(" \   / ")
+            print("  \ /  ")
+            print("   0   ")
+            print("  /|\  ")
+            print("  / \  ")
+            print()
+            print("^^^^^^^")
+        elif self.strikes == 3:
+            print("  \ /  ")
+            print("   0   ")
+            print("  /|\  ")
+            print("  / \  ")
+            print()
+            print("^^^^^^^")
+        else:
+            print("   X   ")
+            print("  /|\  ")
+            print("  / \  ")
+            print()
+            print("^^^^^^^")
