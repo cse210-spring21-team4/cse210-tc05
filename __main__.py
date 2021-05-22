6fce1a11efc5980bd96aa646122ecf63276cfbd8
Sarah Adams <sarahadams2718@gmail.com>
2021-05-21 21:12:47 -0600

    update

--- __main__.py
+++ __main__.py
@@ -7,8 +7,14 @@ class Word_Select:
         word_file = open("wordlist.10000.txt", "r")
         content = word_file.read()
         content_list = content.split()
-        # returns 1 picked word 
-        return random.sample(content_list,1)
+        # picks a word
+        picked_word = random.sample(content_list,1)
+        # converts list length 1 into a string
+        final_word = ""
+        for ele in picked_word:
+            final_word = ele
+        # returns string
+        return final_word
 
 
 
