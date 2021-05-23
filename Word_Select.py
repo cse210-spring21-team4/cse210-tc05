a07a962490351851fd6b4cf88aca6a3a2ce406ff
Sarah Adams <sarahadams2718@gmail.com>
2021-05-22 20:25:58 -0600

    moving word_select to a py file

---
+++ Word_Select.py
@@ -0,0 +1,26 @@
+import random
+
+class Word_Select:
+
+    def Word_Select():
+        # reads the file and splits it into a list
+        word_file = open("wordlist.10000.txt", "r")
+        content = word_file.read()
+        content_list = content.split()
+        # picks a word
+        picked_word = random.sample(content_list,1)
+        # converts list length 1 into a string
+        final_word = ""
+        for ele in picked_word:
+            final_word = ele
+        # returns string
+        return final_word
+
+
+
+
+
+
+
+
+
